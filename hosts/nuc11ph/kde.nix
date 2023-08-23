5d90d07e63ac9ab4881d2bdc1a3dcd3ef3e4789e
idev1669 <id3v1669@gmail.com>
2023-08-23 10:40:28 +0800

    add kde entry

---
+++ hosts/nuc11ph/kde.nix
@@ -0,0 +1,93 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ hyprland, lib, config, pkgs, ... }:
+
+{
+  imports =[
+    ./hw.nix
+    ./../universal.nix
+  ];
+
+  security = {
+    polkit.enable = true;
+  };
+  programs = {
+    regreet.enable = true;
+    dconf.enable = true;
+    bash = {
+      interactiveShellInit = ''
+        if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+          Hyprland
+        fi
+      '';
+    };
+  };
+
+  systemd = {
+  user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+  xdg = {
+    autostart.enable = true;
+    portal = {
+      enable = true;
+      wlr.enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+    };
+  };
+  services = {
+    gvfs.enable = true; # Mount, trash, and other functionalities
+    mpd.enable = true;
+    greetd = {
+      enable = true;
+      settings = {
+        initial_session = {
+          user = "user";
+          command = "$SHELL -l";
+        };
+      };
+    };
+  };
+  
+  environment = {
+    systemPackages = with pkgs; [
+      polkit_gnome
+      xorg.xhost
+    ];
+    sessionVariables = {
+      EDITOR = "nano";
+      BROWSER = "firefox";
+      TERMINAL = "alacritty";
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME= "nvidia";
+      LIBVA_DRIVER_NAME = "nvidia";
+      __GL_VRR_ALLOWED = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      WLR_RENDERER_ALLOW_SOFTWARE = "1";
+      CLUTTER_BACKEND = "wayland";
+      WLR_RENDERER = "vulkan";
+      NIXOS_OZONE_WL = "1";
+      GDK_BACKEND = "wayland,x11";
+
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+    };
+  };
+}
