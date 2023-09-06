11ec3923fa1d6177afe109706dd49db4578c0fc7
idev1669 <id3v1669@gmail.com>
2023-09-06 12:53:19 +0800

    refactoring ...

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, nur, ... }: 
+  outputs = { self, nixpkgs, home-manager, hyprland, nur, ... }@inputs: 
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -30,89 +30,110 @@
         allowUnfree = true;
       };     
     };
+    mkSystem = pkgs: system: hostname: envir:
+      pkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ 
+          (./. + "/hosts/${hostname}/${envir}.nix")
+          hyprland.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.user = import (./. + "/home/home${hostname}${envir}.nix") ;
+              extraSpecialArgs = { inherit inputs; };
+            };
+            nixpkgs.overlays = [
+              nur.overlay
+            ];
+          }
+        ];
+      };
   in {
     nixosConfigurations = {
-      nuc11phhypr = nixpkgs.lib.nixosSystem rec {
-        inherit system;
-        specialArgs = { inherit hyprland; };
-        modules = [ 
-          ./hosts/nuc11ph/hyprland.nix
-          hyprland.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.user = import ./home/homenuc11phhypr.nix ;
-            home-manager.extraSpecialArgs = specialArgs;
-          }
-        ];
-      };
-      nuc11phkde = nixpkgs.lib.nixosSystem rec {
-        inherit system;
-        modules = [ 
-          ./hosts/nuc11ph/kde.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.user = import ./home/homenuc11phkde.nix ;
-          }
-        ];
-      };
-      nuc11phgnome = nixpkgs.lib.nixosSystem rec {
-        inherit system;
-        modules = [ 
-          ./hosts/nuc11ph/gnome.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.user = import ./home/homenuc11phgnome.nix ;
-          }
-        ];
-      };
-      l14g3hypr = nixpkgs.lib.nixosSystem rec {
-        inherit system;
-        specialArgs = { inherit hyprland; };
-        modules = [ 
-          ./hosts/l14g3/hyprland.nix
-          hyprland.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.user = import ./home/homel14g3hypr.nix ;
-            home-manager.extraSpecialArgs = specialArgs;
-          }
-        ];
-      };
-      l14g3gnome = nixpkgs.lib.nixosSystem rec {
-        inherit system;
-        modules = [ 
-          ./hosts/l14g3/gnome.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.user = import ./home/homel14g3gnome.nix ;
-          }
-        ];
-      };
-      alexpchypr = nixpkgs.lib.nixosSystem rec {
-        inherit system;
-        specialArgs = { inherit hyprland; };
-        modules = [ 
-          ./hosts/alexpc/hyprland.nix
-          hyprland.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.user = import ./home/homealexpchypr.nix ;
-            home-manager.extraSpecialArgs = specialArgs;
-          }
-        ];
-      };
+      nuc11phhypr = mkSystem inputs.nixpkgs system "nuc11ph" "hypr";
+      # nuc11phhypr = nixpkgs.lib.nixosSystem rec {
+      #   inherit system;
+      #   specialArgs = { inherit hyprland; };
+      #   modules = [ 
+      #     ./hosts/nuc11ph/hyprland.nix
+      #     hyprland.nixosModules.default
+      #     home-manager.nixosModules.home-manager
+      #     {
+      #       home-manager.useGlobalPkgs = true;
+      #       home-manager.useUserPackages = true;
+      #       home-manager.users.user = import ./home/homenuc11phhypr.nix ;
+      #       home-manager.extraSpecialArgs = specialArgs;
+      #     }
+      #   ];
+      # };
+      # nuc11phkde = nixpkgs.lib.nixosSystem rec {
+      #   inherit system;
+      #   modules = [ 
+      #     ./hosts/nuc11ph/kde.nix
+      #     home-manager.nixosModules.home-manager
+      #     {
+      #       home-manager.useGlobalPkgs = true;
+      #       home-manager.useUserPackages = true;
+      #       home-manager.users.user = import ./home/homenuc11phkde.nix ;
+      #     }
+      #   ];
+      # };
+      # nuc11phgnome = nixpkgs.lib.nixosSystem rec {
+      #   inherit system;
+      #   modules = [ 
+      #     ./hosts/nuc11ph/gnome.nix
+      #     home-manager.nixosModules.home-manager
+      #     {
+      #       home-manager.useGlobalPkgs = true;
+      #       home-manager.useUserPackages = true;
+      #       home-manager.users.user = import ./home/homenuc11phgnome.nix ;
+      #     }
+      #   ];
+      # };
+      # l14g3hypr = nixpkgs.lib.nixosSystem rec {
+      #   inherit system;
+      #   specialArgs = { inherit hyprland; };
+      #   modules = [ 
+      #     ./hosts/l14g3/hyprland.nix
+      #     hyprland.nixosModules.default
+      #     home-manager.nixosModules.home-manager
+      #     {
+      #       home-manager.useGlobalPkgs = true;
+      #       home-manager.useUserPackages = true;
+      #       home-manager.users.user = import ./home/homel14g3hypr.nix ;
+      #       home-manager.extraSpecialArgs = specialArgs;
+      #     }
+      #   ];
+      # };
+      # l14g3gnome = nixpkgs.lib.nixosSystem rec {
+      #   inherit system;
+      #   modules = [ 
+      #     ./hosts/l14g3/gnome.nix
+      #     home-manager.nixosModules.home-manager
+      #     {
+      #       home-manager.useGlobalPkgs = true;
+      #       home-manager.useUserPackages = true;
+      #       home-manager.users.user = import ./home/homel14g3gnome.nix ;
+      #     }
+      #   ];
+      # };
+      # alexpchypr = nixpkgs.lib.nixosSystem rec {
+      #   inherit system;
+      #   specialArgs = { inherit hyprland; };
+      #   modules = [ 
+      #     ./hosts/alexpc/hyprland.nix
+      #     hyprland.nixosModules.default
+      #     home-manager.nixosModules.home-manager
+      #     {
+      #       home-manager.useGlobalPkgs = true;
+      #       home-manager.useUserPackages = true;
+      #       home-manager.users.user = import ./home/homealexpchypr.nix ;
+      #       home-manager.extraSpecialArgs = specialArgs;
+      #     }
+      #   ];
+      # };
     };
   };
 }
