7e2fd1f6e477295559fd5519abbe7bbb4442dce5
idev1669 <id3v1669@gmail.com>
2023-09-07 23:59:42 +0800

    mic script

---
+++ ewwmy/scripts/mic.sh
@@ -0,0 +1,19 @@
+#!/run/current-system/sw/bin/bash
+
+cards=$(aplay -l | grep card | awk -F: '{print $1}' | awk '{print $2}' | sort -u)
+
+for card in $cards; do
+  controls=$(amixer -c $card scontrols)
+  while IFS= read -r line; do
+    echo "Line before if: $line"
+    if [[ $line == *Mic* ]]; then
+      echo "Line after if: $line"
+      control=$(echo "$line" | awk -F"'" '{print $2}')
+      echo "Control: $control"
+      for i in 0 1; do
+        echo "amixer -c $card sset \"$control,$i\" toggle"
+        amixer -c $card sset "$control,$i" toggle
+      done
+    fi
+  done <<< "$controls"
+done
