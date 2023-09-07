1712709660f22c42579e1dd13c6b554d4a4c019a
idev1669 <id3v1669@gmail.com>
2023-09-07 21:06:08 +0800

    enable fontconfig for hm

--- home/home.nix
+++ home/home.nix
@@ -21,87 +21,87 @@
     homeDirectory = "/home/${uservars.name}";
     fonts.fontconfig.enable = true;
     packages = (with pkgs; [
-    #screenshots
-    grim
-    slurp
+      #screenshots
+      grim
+      slurp
 
-    #sound
-    pamixer
-    pavucontrol
+      #sound
+      pamixer
+      pavucontrol
 
-    #vm and virt
-    virt-manager
-    distrobox
-    docker
-    docker-compose
-    egl-wayland
+      #vm and virt
+      virt-manager
+      distrobox
+      docker
+      docker-compose
+      egl-wayland
 
-    #web
-    curl
-    wget
-    wireshark
-    nm-tray
+      #web
+      curl
+      wget
+      wireshark
+      nm-tray
 
-    #vid photo sound etc
-    krita
-    spotify
-    ffmpeg
-    vlc
-    mpc-cli
-    pamixer
-    nomacs
-    gnome.eog
+      #vid photo sound etc
+      krita
+      spotify
+      ffmpeg
+      vlc
+      mpc-cli
+      pamixer
+      nomacs
+      gnome.eog
 
-    #chat
-    telegram-desktop
-    whatsapp-for-linux
+      #chat
+      telegram-desktop
+      whatsapp-for-linux
 
-    #utils
-    partition-manager
-    qbittorrent
-    pciutils
-    usbutils
-    lshw
-    file
-    gnome.nautilus
-    rustdesk
-    btop
-    neofetch
-    git
-    gh
-    github-desktop
-    iwd
-    zip
-    unzip
+      #utils
+      partition-manager
+      qbittorrent
+      pciutils
+      usbutils
+      lshw
+      file
+      gnome.nautilus
+      rustdesk
+      btop
+      neofetch
+      git
+      gh
+      github-desktop
+      iwd
+      zip
+      unzip
 
-    #security
-    openssl
-    veracrypt
-    
+      #security
+      openssl
+      veracrypt
 
-    #hw related
-    bluez
-    mesa
-    mesa-demos
+      #hw related
+      bluez
+      mesa
+      mesa-demos
 
-    #games
-    steam
+      #games
+      steam
 
-    #other
-    openjdk19
-    libsForQt5.qt5.qtwayland
-    libsForQt5.qt5ct
-    qt6.qtwayland
-    (callPackage ./custom/xwaylandvideobridge.nix {})
+      #other
+      openjdk19
+      libsForQt5.qt5.qtwayland
+      libsForQt5.qt5ct
+      qt6.qtwayland
+      (callPackage ./custom/xwaylandvideobridge.nix {})
 
-    #temp
-    git
-    kitty
-    wofi
-  ]) ++ lib.lists.optionals (envir == "gnome") (with pkgs.gnomeExtensions;[
-    appindicator
-    notification-banner-reloaded
-    gsconnect
-    quick-settings-tweaker
-  ]);
+      #temp
+      git
+      kitty
+      wofi
+    ]) ++ lib.lists.optionals (envir == "gnome") (with pkgs.gnomeExtensions;[
+      appindicator
+      notification-banner-reloaded
+      gsconnect
+      quick-settings-tweaker
+    ]);
+  };
 }
