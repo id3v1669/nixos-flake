5abb4b33349ad4f70941891bc63a9ecc924d06e4
idev1669 <id3v1669@gmail.com>
2023-09-17 05:53:34 +0800

    rofi conf done

--- home/enviroment/rofi/default.nix
+++ home/enviroment/rofi/default.nix
@@ -1,20 +1,85 @@
 { config, lib, pkgs, ... }: 
-
 {
   programs.rofi = {
     enable = true;
-    #spackage = pkgs.rofi-wayland;
-    #location = "center";
+    #package = pkgs.rofi-wayland;
+    cycle = false;
+    location = "center";
+    font = "JetBrainsMono Nerd Font Medium 10";
     plugins = (with pkgs; [
       rofi-calc 
       rofi-mpd
       rofi-emoji
       rofi-bluetooth
     ]);
-    #extraConfig = {
-    #  modi = "drun,emoji,ssh";
-    #  kb-primary-paste = "Control+V,Shift+Insert";
-    #  kb-secondary-paste = "Control+v,Insert";
-    #};
+    theme = let inherit (config.lib.formats.rasi) mkLiteral; in  
+    {
+      "*" = {
+        bg = mkLiteral "#${config.colorScheme.colors.base00}66";
+        bg-alt = mkLiteral "#${config.colorScheme.colors.base02}66";
+        bg-selected = mkLiteral "#${config.colorScheme.colors.base01}66";
+        fg = mkLiteral "#${config.colorScheme.colors.base05}";
+        fg-alt = mkLiteral "#${config.colorScheme.colors.base08}";
+        border = 0;
+        margin = 0;
+        padding = 0;
+        spacing = 0;
+      };
+      "window" = {
+        width = mkLiteral "30%";
+        background-color = mkLiteral "@bg";
+      };
+      "element" = {
+        padding = mkLiteral "8 12";
+        background-color = mkLiteral "transparent";
+        text-color = mkLiteral "@fg-alt";
+      };
+      "element selected" = {
+        text-color = mkLiteral "@fg";
+        background-color = mkLiteral "@bg-selected";
+      };
+      "element-text" = {
+        background-color = mkLiteral "transparent";
+        text-color = mkLiteral "inherit";
+        vertical-align = mkLiteral "0.5";
+      };
+      "element-icon" = {
+        size = 14;
+        padding = mkLiteral "0 10 0 0";
+        background-color = mkLiteral "transparent";
+      };
+      "entry" = {
+        padding = 12;
+        background-color = mkLiteral "@bg-alt";
+        text-color = mkLiteral "@fg";
+      };
+      "inputbar" = {
+        children = map mkLiteral ["prompt" "entry"];
+        background-color = mkLiteral "@bg";
+      };
+      "listview" = {
+        background-color = mkLiteral "@bg";
+        columns = 1;
+        lines = 10;
+      };
+      "mainbox" = {
+        children = map mkLiteral ["inputbar" "listview"];
+        background-color = mkLiteral "@bg";
+      };
+      "prompt" = {
+      enabled = true;
+      padding = mkLiteral "12 0 0 12";
+        background-color = mkLiteral "@bg-alt";
+        text-color = mkLiteral "@fg";
+      };
+    };
+    extraConfig = {
+      modi = "drun,emoji,ssh";
+      kb-primary-paste = "Control+V,Shift+Insert";
+      kb-secondary-paste = "Control+v,Insert";
+      display-drun = "Applications:";
+      display-window = "Windows:";
+      drun-display-format = "{name}";
+    };
   };
 }
