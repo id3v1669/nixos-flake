0aa5a9416ac0108053409bfdd4907cebb378a7c7
id3v1669 <id3v1669@gmail.com>
2023-11-08 01:24:34 +0800

    srv bump7

--- hosts/configuration/default.nix
+++ hosts/configuration/default.nix
@@ -18,36 +18,39 @@
     defaultSopsFile = ./../../secrets/secret.enc.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/${uservars.name}/.config/sops/age/keys.txt";
-    secrets."myservice/my_subdir/my_secret" = {
-      owner = "sometestservice";
+    secrets."nextcloud/db-password" = {
+      owner = "${uservars.name}";
+    };
+    secrets."nextcloud/admin-password" = {
+      owner = "${uservars.name}";
     };
     secrets.example-key = {
       mode = "0440";
       owner = "${uservars.name}";
     };
   };
-  systemd.services."sometestservice" = {
-    script = ''
-        echo "
-        Hey bro! I'm a service, and imma send this secure password:
-        $(cat ${config.sops.secrets."myservice/my_subdir/my_secret".path})
-        located in:
-        ${config.sops.secrets."myservice/my_subdir/my_secret".path}
-        to database and hack the mainframe
-        " > /var/lib/sometestservice/testfile
-      '';
-    serviceConfig = {
-      User = "sometestservice";
-      WorkingDirectory = "/var/lib/sometestservice";
-    };
-  };
-  users.users.sometestservice = {
-    home = "/var/lib/sometestservice";
-    createHome = true;
-    isSystemUser = true;
-    group = "sometestservice";
-  };
-  users.groups.sometestservice = { };
+  # systemd.services."sometestservice" = {
+  #   script = ''
+  #       echo "
+  #       Hey bro! I'm a service, and imma send this secure password:
+  #       $(cat ${config.sops.secrets."myservice/my_subdir/my_secret".path})
+  #       located in:
+  #       ${config.sops.secrets."myservice/my_subdir/my_secret".path}
+  #       to database and hack the mainframe
+  #       " > /var/lib/sometestservice/testfile
+  #     '';
+  #   serviceConfig = {
+  #     User = "sometestservice";
+  #     WorkingDirectory = "/var/lib/sometestservice";
+  #   };
+  #};
+  # users.users.sometestservice = {
+  #   home = "/var/lib/sometestservice";
+  #   createHome = true;
+  #   isSystemUser = true;
+  #   group = "sometestservice";
+  # };
+  # users.groups.sometestservice = { };
   networking = {
     hostName = "${hostname}${envir}";
     useDHCP = lib.mkDefault true;
