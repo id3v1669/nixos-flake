e608dc6b527e6d84ea2e6f7c327ad329749a9586
id3v1669 <id3v1669@gmail.com>
2023-11-09 11:12:25 +0800

    srv bump17

--- hosts/configuration/srvnet510.nix
+++ hosts/configuration/srvnet510.nix
@@ -9,58 +9,58 @@
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
   #networking.firewall.allowedTCPPorts = [ 22 80 443 8080 8090 28943 ];
   networking.firewall.enable = false;
-  security.acme = {
-    defaults.email = "nico@nico.ni";
-    acceptTerms = true;
-  };
-  services = {
-    nextcloud = {
-      enable = true;
-      package = pkgs.nextcloud27;
-      database.createLocally = true;
-      https = true;
-      nginx.hstsMaxAge = 31536000;
-      configureRedis = true;
-      caching.redis = true;
-      webfinger = true;
-      maxUploadSize = "10G";
-      autoUpdateApps = {
-        enable = true;
-        startAt = "Sun 14:00:00";
-      };
-      extraApps = with config.services.nextcloud.package.packages.apps; {
-        inherit calendar tasks;
-      };
-      extraAppsEnable = true;
-      hostName = "nextcloud.id3v1669.com";
-      config = {
-        dbtype = "pgsql";
-        defaultPhoneRegion = "AU";
-        adminuser = "nextcloudadmin";
-        adminpassFile = "/etc/nextcloud-admin-pass";
-        extraTrustedDomains = [ "nextcloud.id3v1669.com" ];
-      };
-    };
-    nginx = {
-      enable = true;
-        recommendedGzipSettings = true;
-        recommendedOptimisation = true;
-        recommendedProxySettings = true;
-        recommendedTlsSettings = true;
-        virtualHosts."nextcloud.id3v1669.com" = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/".root = pkgs.nextcloud27;
-      };
-    };
-    redis.servers.nextcloud = {
-      enable = true;
-      port = 31638;
-      bind = "127.0.0.1";
-    };
-  };
-  systemd.services."nextcloud-setup" = {
-    requires = [ "postgresql.service" ];
-    after = [ "postgresql.service" ];
-  };
+  # security.acme = {
+  #   defaults.email = "nico@nico.ni";
+  #   acceptTerms = true;
+  # };
+  # services = {
+  #   nextcloud = {
+  #     enable = true;
+  #     package = pkgs.nextcloud27;
+  #     database.createLocally = true;
+  #     https = true;
+  #     nginx.hstsMaxAge = 31536000;
+  #     configureRedis = true;
+  #     caching.redis = true;
+  #     webfinger = true;
+  #     maxUploadSize = "10G";
+  #     autoUpdateApps = {
+  #       enable = true;
+  #       startAt = "Sun 14:00:00";
+  #     };
+  #     extraApps = with config.services.nextcloud.package.packages.apps; {
+  #       inherit calendar tasks;
+  #     };
+  #     extraAppsEnable = true;
+  #     hostName = "nextcloud.id3v1669.com";
+  #     config = {
+  #       dbtype = "pgsql";
+  #       defaultPhoneRegion = "AU";
+  #       adminuser = "nextcloudadmin";
+  #       adminpassFile = "/etc/nextcloud-admin-pass";
+  #       extraTrustedDomains = [ "nextcloud.id3v1669.com" ];
+  #     };
+  #   };
+  #   nginx = {
+  #     enable = true;
+  #       recommendedGzipSettings = true;
+  #       recommendedOptimisation = true;
+  #       recommendedProxySettings = true;
+  #       recommendedTlsSettings = true;
+  #       virtualHosts."nextcloud.id3v1669.com" = {
+  #       enableACME = true;
+  #       forceSSL = true;
+  #       locations."/".root = pkgs.nextcloud27;
+  #     };
+  #   };
+  #   redis.servers.nextcloud = {
+  #     enable = true;
+  #     port = 31638;
+  #     bind = "127.0.0.1";
+  #   };
+  # };
+  # systemd.services."nextcloud-setup" = {
+  #   requires = [ "postgresql.service" ];
+  #   after = [ "postgresql.service" ];
+  # };
 }
