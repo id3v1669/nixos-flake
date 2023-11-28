a6703eb331a9fe121eed0577cbaec82c66515291
id3v1669 <id3v1669@gmail.com>
2023-11-29 01:18:51 +0800

    fix

--- home/wallpapers/default.nix
+++ home/wallpapers/default.nix
@@ -1,4 +1,4 @@
 { config, lib, desk, ... }:{
-  home.file."Pictures/Wallpapers/default1.png".source = ./. + "/../../assets/wallapers/default1.png";
-  home.file."Pictures/Wallpapers/default2.png".source = ./. + "/../../assets/wallapers/default2.png";
+  home.file."Pictures/Wallpapers/default1.png".source = ./. + "/../../assets/wallpapers/default1.png";
+  home.file."Pictures/Wallpapers/default2.png".source = ./. + "/../../assets/wallpapers/default2.png";
 }
