2dbe8d350b87c599d87e90d65d31ce2a3948fc43
id3v1669 <id3v1669@gmail.com>
2023-11-30 12:03:23 +0800

    l14g3 sway6

--- scripts/workspaces.nix
+++ scripts/workspaces.nix
@@ -2,14 +2,22 @@
 {
   home.file.".scripts/eww_ws.sh" = {
     executable = true;
-    text = ''
+    text = let
+      start = ''
 #!/usr/bin/env bash
 
 wss() {
 
   ws=("")
+      '';
+      env = if envir == "hypr" then ''
   wsa=($(hyprctl workspaces | grep 'workspace ID .*(*)' | awk '{ gsub(/[()]/, "", $3); print $3 }'))
   curindex=($(hyprctl activeworkspace | grep 'workspace ID .*(*)' | awk '{ gsub(/[()]/, "", $3); print $3 }'))
+      '' else  if envir == "sway" then ''
+  wsa=($(swaymsg -t get_workspaces | grep -o '"num":\s*[0-9]\+' | awk '{print $2}'))
+  curindex=($(swaymsg -t get_workspaces -p | grep "focused" | awk '{print $2}'))
+      '' else '''';
+      end = ''
   max=$(printf "%s\n" "''${wsa[@]}" | sort -n | tail -n 1)
   for (( counter=0; counter<max; counter++ )); do
     ws[counter]=""
@@ -22,6 +30,8 @@ wss() {
 
 wss
 eww update wss="''${ws[*]}"
-    '';
+      '';
+      in
+        start + env + end;
   };
 }
