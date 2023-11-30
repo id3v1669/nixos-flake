def131e97fb720deb6cfcf4b23b5572f6ae8833f
id3v1669 <id3v1669@gmail.com>
2023-11-30 09:12:11 +0800

    l14g3 sway conf2

--- home/enviroment/sway/default.nix
+++ home/enviroment/sway/default.nix
@@ -10,7 +10,9 @@
       #bars = [{ command = "${config.home.homeDirectory}/.scripts/ewwlauncher.sh"; }];
       startup = [
         { command = "${config.home.homeDirectory}/.scripts/ewwlauncher.sh"; always = true; }
+        { command = "${config.home.homeDirectory}/.scripts/wallpaper.sh"; always = true; }
         { command = "arrpc &"; always = true; }
+        { command = "swhks &"; always = true; }
       ];
       terminal = "alacritty";
       gaps.inner = 7;
@@ -37,33 +39,33 @@
         mod = config.wayland.windowManager.sway.config.modifier;
       in {
 
-        "Ctrl+Right" = "workspace next";
-        "Ctrl+Left" = "workspace prev";
+        # "Ctrl+Right" = "workspace next";
+        # "Ctrl+Left" = "workspace prev";
 
-        "${mod}+1" = "workspace number 1";
-        "${mod}+2" = "workspace number 2";
-        "${mod}+3" = "workspace number 3";
-        "${mod}+4" = "workspace number 4";
-        "${mod}+5" = "workspace number 5";
-        "${mod}+6" = "workspace number 6";
-        "${mod}+7" = "workspace number 7";
-        "${mod}+8" = "workspace number 8";
-        "${mod}+9" = "workspace number 9";
-        "${mod}+0" = "workspace number 10";
+        # "${mod}+1" = "workspace number 1";
+        # "${mod}+2" = "workspace number 2";
+        # "${mod}+3" = "workspace number 3";
+        # "${mod}+4" = "workspace number 4";
+        # "${mod}+5" = "workspace number 5";
+        # "${mod}+6" = "workspace number 6";
+        # "${mod}+7" = "workspace number 7";
+        # "${mod}+8" = "workspace number 8";
+        # "${mod}+9" = "workspace number 9";
+        # "${mod}+0" = "workspace number 10";
 
-        "${mod}+Shift+q" = "kill";
-        "${mod}+g" = "fullscreen toggle"; 
+        # "${mod}+Shift+q" = "kill";
+        # "${mod}+g" = "fullscreen toggle"; 
 
-        "Ctrl+Shift+1" = "move container to workspace number 1";
-        "Ctrl+Shift+2" = "move container to workspace number 2";
-        "Ctrl+Shift+3" = "move container to workspace number 3";
-        "Ctrl+Shift+4" = "move container to workspace number 4";
-        "Ctrl+Shift+5" = "move container to workspace number 5";
-        "Ctrl+Shift+6" = "move container to workspace number 6";
-        "Ctrl+Shift+7" = "move container to workspace number 7";
-        "Ctrl+Shift+8" = "move container to workspace number 8";
-        "Ctrl+Shift+9" = "move container to workspace number 9";
-        "Ctrl+Shift+0" = "move container to workspace number 10";
+        # "Ctrl+Shift+1" = "move container to workspace number 1";
+        # "Ctrl+Shift+2" = "move container to workspace number 2";
+        # "Ctrl+Shift+3" = "move container to workspace number 3";
+        # "Ctrl+Shift+4" = "move container to workspace number 4";
+        # "Ctrl+Shift+5" = "move container to workspace number 5";
+        # "Ctrl+Shift+6" = "move container to workspace number 6";
+        # "Ctrl+Shift+7" = "move container to workspace number 7";
+        # "Ctrl+Shift+8" = "move container to workspace number 8";
+        # "Ctrl+Shift+9" = "move container to workspace number 9";
+        # "Ctrl+Shift+0" = "move container to workspace number 10";
       };
       colors = {
         background = "#${config.colorScheme.colors.base00}";
