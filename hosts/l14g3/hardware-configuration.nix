a736d9d25ddcdbdb53dd412e5f08714a8f40ad42
id3v1669 <id3v1669@gmail.com>
2023-12-11 00:48:20 +0800

    new hw l14g3

--- hosts/l14g3/hardware-configuration.nix
+++ hosts/l14g3/hardware-configuration.nix
@@ -8,17 +8,17 @@
     initrd = {
       availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
       kernelModules = [ "amdgpu" ];
-      luks.devices."luks-9eb807dd-c147-47d9-8865-8c8281f401f4".device = "/dev/disk/by-uuid/9eb807dd-c147-47d9-8865-8c8281f401f4";
+      luks.devices."luks-85970870-984a-4a67-9cf4-8e11c38a7cac".device = "/dev/disk/by-uuid/85970870-984a-4a67-9cf4-8e11c38a7cac";
     };
   };
 
   fileSystems."/" ={ 
-    device = "/dev/disk/by-uuid/ba2ca1b8-635e-4182-b540-5a991aed0c5c";
+    device = "/dev/disk/by-uuid/4e32d86d-ac91-4a86-a433-0ed7bda6050d";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/0438-5961";
+    device = "/dev/disk/by-uuid/67E3-17ED";
     fsType = "vfat";
   };
 
