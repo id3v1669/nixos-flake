e3fe90a6d9a5629b09390084203ae59f9ad94a0d
id3v1669 <nico@nico.ni>
2024-09-08 07:38:34 +0800

    update lutget for base16x2

--- home/scripts/wallpaper.nix
+++ home/scripts/wallpaper.nix
@@ -17,7 +17,7 @@ writeShellApplication{
   ];
   text = let
   imageFile = ./../../assets/wallpapers/${uservars.wp};
-  clp = config.colorScheme.palette;
+  clp = config.palette;
   launch = if envir == "Hyprland" then ''
 kill "$(pidof hyprpaper)"
 hyprpaper &
@@ -36,7 +36,11 @@ if [ ! -f "/etc/backgrounds/${colorsvar}/$_image" ]; then
   if [ ! -d "/etc/backgrounds/${colorsvar}" ]; then
     mkdir "/etc/backgrounds/${colorsvar}"
   fi
-  lutgen apply ${imageFile} -o /etc/backgrounds/${colorsvar} -- "#ABCDEF" ${clp.base01} ${clp.base04} ${clp.base05} ${clp.base06} ${clp.base07} ${clp.base08} ${clp.base09} ${clp.base0A} ${clp.base0B} ${clp.base0C} ${clp.base0D} ${clp.base0E} ${clp.base0F}
+  lutgen apply ${imageFile} -o /etc/backgrounds/${colorsvar} -- "#ABCDEF" ${clp.base00.hex} ${clp.base01.hex}\
+    ${clp.base04.hex} ${clp.base05.hex} ${clp.base06.hex} ${clp.base07.hex} ${clp.base08.hex} ${clp.base09.hex}\
+    ${clp.base0A.hex} ${clp.base0B.hex} ${clp.base0C.hex} ${clp.base0D.hex} ${clp.base0E.hex} ${clp.base0F.hex}\
+    ${clp.base10.hex} ${clp.base11.hex} ${clp.base14.hex} ${clp.base15.hex} ${clp.base16.hex} ${clp.base17.hex} ${clp.base18.hex}\
+    ${clp.base19.hex} ${clp.base1A.hex} ${clp.base1B.hex} ${clp.base1C.hex} ${clp.base1D.hex} ${clp.base1E.hex} ${clp.base1F.hex}
   mv /etc/backgrounds/${colorsvar}/*$_image /etc/backgrounds/${colorsvar}/$_image
 fi
 
